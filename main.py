f8229fecc5b6eb3ffcf49c2897ea14e7492ca6b9
Florian Krause <florian.krause@fladd.de>
2013-11-14 23:33:59 +0100

    Parse system fonts add them to pygame and set aliases

--- main.py
+++ main.py
@@ -20,22 +20,50 @@ def main():
                 for pyfile in glob.glob(path):
                     projects[os.path.split(pyfile)[-1]] = pyfile
 
-    expyriment.stimuli.defaults.textline_text_font = \
-            os.path.abspath("fonts/FreeSans.ttf")
-    expyriment.stimuli.defaults.textbox_text_font = \
-            os.path.abspath("fonts/FreeSans.ttf")
-    expyriment.stimuli.defaults.textscreen_heading_font = \
-            os.path.abspath("fonts/FreeSans.ttf")
-    expyriment.stimuli.defaults.textscreen_text_font = \
-            os.path.abspath("fonts/FreeSans.ttf")
-    expyriment.io.defaults.textinput_message_font = \
-            os.path.abspath("fonts/FreeSans.ttf")
-    expyriment.io.defaults.textinput_user_text_font = \
-            os.path.abspath("fonts/FreeMono.ttf")
-    expyriment.io.defaults.menu_heading_font = \
-            os.path.abspath("fonts/FreeSans.ttf")
-    expyriment.io.defaults.menu_text_font = \
-            os.path.abspath("fonts/FreeMono.ttf")
+    pygame.font.init()
+    for font in glob.glob("/system/fonts/*"):
+        if font[-4:].lower() in ['.ttf', '.ttc']:
+            font = unicode(font)
+            name = os.path.split(font)[1]
+            bold = name.find('Bold') >= 0
+            italic = name.find('Italic') >= 0
+            oblique = name.find('Oblique') >= 0
+            name = name.replace(".ttf", "")
+            if name.endswith("Regular"):
+                name = name.replace("Regular", "")
+            if name.endswith("Bold"):
+                name = name.replace("Bold", "")
+            if name.endswith("Italic"):
+                name = name.replace("Italic", "")
+            if name.endswith("Oblique"):
+                name = name.replace("Oblique", "")
+            name = ''.join([c.lower() for c in name if c.isalnum()])
+            pygame.sysfont._addfont(name, bold, italic or oblique, font,
+                                    pygame.sysfont.Sysfonts)
+                                    
+    aliases = (
+        ('monospace', 'misc-fixed', 'courier', 'couriernew', 'console',
+         'fixed', 'mono', 'freemono', 'bitstreamverasansmono',
+         'verasansmono', 'monotype', 'lucidaconsole', 'droidsansmono),
+        ('sans', 'arial', 'helvetica', 'swiss', 'freesans',
+         'bitstreamverasans', 'verasans', 'verdana', 'tahoma', 'droidsans),
+        ('serif', 'times', 'freeserif', 'bitstreamveraserif', 'roman',
+         'timesroman', 'timesnewroman', 'dutch', 'veraserif',
+         'georgia', 'droidserif'),
+    )
+    for set in aliases:
+        found = None
+        fname = None
+        for name in set:
+            if name in pygame.sysfont.Sysfonts:
+                found = pygame.sysfont.Sysfonts[name]
+                fname = name
+                break
+        if not found:
+            continue
+        for name in set:
+            if name not in pygame.sysfont.Sysfonts:
+                pygame.sysfont.Sysalias[name] = found
 
     expyriment.control.defaults.event_logging = 0
     exp = expyriment.control.initialize()
