284f0f8075a900962ffe7582b329ad45fa54bf35
Oliver Lindemann <lindemann@cognitive-psychology.eu>
2014-03-12 19:09:46 +0100

    update example

--- examples/bisecting_judd.py
+++ examples/bisecting_judd.py
@@ -10,19 +10,35 @@ see e.g.: Bruno N, Bernardis P, Gentilucci M (2008) Visually guided pointing,
     Neurosci Biobehav Rev 32:423–437. doi:10.1016/j.neubiorev.
     2007.08.006
 
-(c) Oliver Lindemann, 2014
+(c) Oliver Lindemann & Florian Krause, 2014
 
 """
+## TODO: requires 0.7.0+ (plus mouse_quit_events)
 
 from expyriment import design, control, stimuli, io, misc
+from expyriment.design.randomize import rand_int
 
 control.set_develop_mode(True)
 
 # settings and defaults
 design.defaults.experiment_background_colour = misc.constants.C_GREY
 design.defaults.experiment_foreground_colour = misc.constants.C_BLACK
+stimulus_delay = 500
 
+### Design ###
 exp = design.Experiment(name="bisecting judd")
+bl = design.Block()
+for arrow in ["left", "right", "no"]:
+    for length in [200, 300, 400]:
+        tr = design.Trial()
+        tr.set_factor("arrow", arrow)
+        tr.set_factor("length", length)
+        bl.add_trial(tr, copies=10)
+bl.shuffle_trials()
+exp.add_block(bl)
+
+exp.data_variable_names = ["trial", "length", "arrow", "pos_x", "pos_y",
+                "mark_x", "mark_y", "RT"]
 
 ### Initialize ####
 control.initialize(exp)
@@ -41,19 +57,17 @@ blank = stimuli.BlankScreen()
 blank.preload()
 
 exp.mouse.show_cursor()
-
-def judd_stimulus(line_length, position, arrows,
+exp.mouse.mouse_quit_event = True
+def judd_stimulus(line_length, arrows,
                   line_width=3, heigth=60, gap=40):
     """Returns a Judd illusion stimulus.
     Line center is the stimulus canvas center.
+    Position will be randomliy selected.
 
     Parameters
     ----------
     line_length: integer
         the length of the line
-    position: integer
-        the position of the stimulus. Stimulus canvas center is also line
-        center
     arrows: tuple of boolean
         boolean indicate the presence of arrows head at both side [left, right]
     line_width: integer, optional
@@ -65,8 +79,14 @@ def judd_stimulus(line_length, position, arrows,
         large the gaps more angular are the arrow heads and tails
 
     """
-
-    rtn = stimuli.Canvas(size=(line_length+2*gap, heigth), position=position)
+    canvas_size = (line_length+2*gap, heigth)
+    x_range = (-exp.screen.center_x + canvas_size[0]/2 + 10,
+                exp.screen.center_x - canvas_size[0]/2 - 10)
+    y_range = (-exp.screen.center_y + canvas_size[1]/2 + 10,
+                exp.screen.center_y - canvas_size[1]/2 - 10)
+    position = (rand_int(x_range[0], x_range[1]),
+                rand_int(y_range[0], y_range[1]))
+    rtn = stimuli.Canvas(size=canvas_size, position=position)
     line_ends = ((-line_length/2,0),(line_length/2,0))
     h2 = heigth/2
     arrow_ends = [[line_ends[0][0]-gap,  h2], [line_ends[1][0]+gap,  h2],
@@ -89,12 +109,38 @@ def judd_stimulus(line_length, position, arrows,
                 line_width=line_width, anti_aliasing=10).plot(rtn)
     return rtn
 
+def run_trial(trial, trial_cnt):
+    blank.present()
+    if trial.get_factor("arrow")=="left":
+        arrows = [True, False]
+    elif trial.get_factor("arrow")=="right":
+        arrows = [False, True]
+    else:
+        arrows = [True, True]
+    exp.clock.wait(stimulus_delay)
+    stim = judd_stimulus(line_length=trial.get_factor("length"),
+                            arrows=arrows)
+    stim.present()
+    exp.clock.reset_stopwatch()
+    rect = stimuli.Rectangle(size=stim.size, position=stim.position)
+    while True:
+        btn_id, click_point, _rt = exp.mouse.wait_press()
+        if btn_id is None:
+            click_point = (-1, -1)
+            break
+        if rect.overlapping_with_position(click_point):
+            break
+    rt = exp.clock.stopwatch_time
+    blank.present()
+    exp.data.add([trial_cnt, trial.get_factor("length"),
+            trial.get_factor("arrow"), stim.position[0], stim.position[1],
+            click_point[0], click_point[1], rt])
+
 ### Start ###
 control.start()
 
-stim = judd_stimulus(line_length=300, position=(0,0), arrows=[True, False])
-stim.present()
-exp.keyboard.wait()
+for cnt, trial in enumerate(exp.blocks[0].trials):
+    run_trial(trial, cnt)
 
 ### End ###
 control.end()
